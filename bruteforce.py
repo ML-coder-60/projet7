8ab1fbaefd1acc09b2097c1b17c84342c779c03e
marc <marc@Formation.home>
2021-09-26 10:06:09 +0200

    Update Bruteforce and add optimized

--- bruteforce.py
+++ bruteforce.py
@@ -1,4 +1,5 @@
 import csv
+import tracemalloc
 from itertools import combinations
 
 MAX_SPENT = 500
@@ -8,10 +9,9 @@ def read_data(csv_file_pah):
     """
     Function read File csv and return data array
     :param
-        - csvFilePah: path file csv
-
+        - csvFilePah: string => path file csv
     :return:
-        - result array to array(data action)
+        - result array => array to array(data action)
     """
     result = []
     try:
@@ -22,7 +22,7 @@ def read_data(csv_file_pah):
     with f:
         reader = csv.reader(f)
         for rows in reader:
-            result.append(rows)
+            result.append([rows[0], int(rows[1]), int(rows[2])])
     return result
 
 
@@ -32,8 +32,8 @@ def combinations_array(data_action):
     :param
         - data_action: array to array (data actions)
     :return:
-        - result:  return all combinations for list data_action
-                   [['name','price','interest'],....],['name','price','interest'],....]]
+        - result:  array => return all combinations for list data_action
+                   [[()], [(['name','price','interest'],....]),(['name','price','interest'],....])]
     """
     result = []
     for nbr_elements in range(len(data_action)+1):
@@ -42,19 +42,36 @@ def combinations_array(data_action):
     return result
 
 
+def combinations2_array(data_action):
+    """
+    All possible combinations of the elements of a given list
+    :param
+        - data_action: array to array (data actions)
+    :return:
+        - result:  array => return all combinations for list data_action
+                   [['name','price','interest'],....],['name','price','interest'],....]]
+    """
+    result = []
+    if len(data_action) == 0:
+        return [[]]
+    for c in combinations2_array(data_action[1:]):
+        result += [c, c + [data_action[0]]]
+    return result
+
+
 def check_max_spent(actions_list):
     """
     :param
-        - actions_list:  actions list [['name','price','interest'],....]
+        - actions_list: array =>  actions list [['name','price','interest'],....]
     :return:
         if sum action is > MAX_SPENT
-            return False
+            return  boolean =>  False
         else
-            return sum price of all action
+            return  integer => sum price of all action
     """
     sum_action = 0
     for actions in actions_list:
-        sum_action += int(actions[1])
+        sum_action += actions[1]
         if sum_action > MAX_SPENT:
             return False
     return sum_action
@@ -64,34 +81,43 @@ def sum_interest(actions_list):
     """
     return the sum interest of all actions
         :param
-            - actions_list: actions list [['name','price','interest'],....]
+            - actions_list: array  => actions list [['name','price','interest'],....]
         :return:
-            -  interest float
+            -  interest:  float
     """
     interest = 0
     for actions in actions_list:
-        interest += int(actions[1])*int(actions[2])/100
+        interest += actions[1]*actions[2]
     return interest
+
+
+def demo_nbr_combination(list_combinations):
+    """ Demo nbr combination """
+    print("nbr combination : {} with  {} selected action".format(
+        len(list_combinations),
+        len(list_combinations[0]))
+    )
 
 
 def find_best_combinations(all_combinations):
     """
     Return best combination, sum spent of combination, gain of combination
     :param
-        - all_combinations:  [['name','price','interest'],....],['name','price','interest'],....]]
+        - all_combinations: array  [['name','price','interest'],....],['name','price','interest'],....]]
     :return:
-        - result: best combination action
-        - sum_spent: sum spent for best combination action
-        - interest_max: gain for best combination action
+        - result: array => best combination action
+        - sum_spent: integer => sum spent for best combination action
+        - interest_max: float => gain for best combination action
     """
     interest_max = 0
     sum_spent = 0
     result = []
+    total_combination = 0
     for actions_combinations in all_combinations:
+        """ Demo nbr combination "
+            demo_nbr_combination(actions_combinations)
         """
-        debug
-        print("nbr combination : {} avec  {} action sélectionée ".format(len(combinations), len(combinations[0])))
-        """
+        total_combination += len(actions_combinations)
         for list_action in actions_combinations:
             somme_action = check_max_spent(list_action)
             if somme_action:
@@ -100,24 +126,49 @@ def find_best_combinations(all_combinations):
                     interest_max = interest
                     result = list_action
                     sum_spent = somme_action
-    return result, sum_spent, interest_max
+    """ Demo nbr combination  """
+    print("\n Total Combination: {}\n".format(total_combination))
+    return result, sum_spent, interest_max/100
 
 
 def display_best_combination(list_action, spent_action, gain_action):
-    margin = 10
+    """
+    Display best combination
+    :param
+        list_action: array best list actions
+    :param
+        spent_action:  integer
+    :param
+        gain_action: float
+    """
+    margin = 15
     s = "Actions".ljust(margin, " ")+"Price".ljust(margin, " ")+"%".ljust(margin, " ")
     for action in list_action:
-        s += "\n"+action[0].split("-")[1].ljust(margin, " ")+action[1].ljust(margin, " ")+action[2].ljust(margin, " ")
+        s += "\n"+action[0].split("-")[1].ljust(margin, " ")
+        s += str(action[1]).ljust(margin, " ")
+        s += str(action[2]).ljust(margin, " ")
     s += "\n\nMoney spent on stocks: {}".format(spent_action)
-    s += "\nTotal gain: {} ".format(gain_action)
+    s += "\nTotal gain Net: {}".format(gain_action)
+    s += "\nTotal gain Brut: {}".format(gain_action+spent_action)
     print(s)
 
 
-""" Read Data"""
-data = read_data('actions.csv')
-""" List all combination """
-combinations_actions = combinations_array(data)
-""" Return best combination, sum spent of combination, gain of combination"""
-best_combination, spent, gain = find_best_combinations(combinations_actions)
-""" Display best combination, sum spent, gain """
-display_best_combination(best_combination, spent, gain)
+def main():
+    tracemalloc.start()
+    """ Read Data"""
+    data = read_data('actions.csv')
+    """ List all combination """
+    combinations_actions = combinations_array(data)
+    """ Return best combination, sum spent of combination, gain of combination"""
+    best_combination, spent, gain = find_best_combinations(combinations_actions)
+    """ Display best combination, sum spent, gain """
+    display_best_combination(best_combination, spent, gain)
+    snapshot = tracemalloc.take_snapshot()
+    top_stats = snapshot.statistics('lineno')
+    print("\n[ Top 4 memory usage ]\n")
+    for stat in top_stats[:4]:
+        print(stat)
+
+
+if __name__ == '__main__':
+    main()
---
+++ optimized.py
@@ -0,0 +1,119 @@
+import csv
+import tracemalloc
+
+MAX_SPENT = 500
+
+
+def read_data(csv_file_pah):
+    """
+    Function read File csv and return data array
+    :param
+        - csvFilePah: string => path file csv
+    :return:
+        - result array => array to array(data action)
+    """
+    result = []
+    try:
+        f = open(csv_file_pah, encoding='utf-8')
+    except OSError:
+        print("Could not open/read file:{}".format(csv_file_pah))
+        return result
+    with f:
+        reader = csv.reader(f)
+        for rows in reader:
+            result.append([rows[0], int(rows[1]), int(rows[2])])
+    return result
+
+
+def check_max_spent(actions_list):
+    """
+    :param
+        - actions_list: array =>  actions list [['name','price','interest'],....]
+    :return:
+        if sum action is > MAX_SPENT
+            return  boolean =>  False
+        else
+            return  integer => sum price of all action
+    """
+    sum_action = 0
+    for actions in actions_list:
+        sum_action += int(actions[1])
+        if sum_action > MAX_SPENT:
+            return False
+    return sum_action
+
+
+def find_best_combinations(max_spent, actions):
+    """
+    Return best combination, sum spent of combination, gain of combination
+    :param
+        - max_spent : Integer
+        - actions :  Array  [['name','price','interest'],....],['name','price','interest'],....]]
+    :return:
+        - result: Array => best combination action
+        - sum_spent: Integer => sum spent for best combination action
+        - interest_max: Float => gain for best combination action
+    """
+
+    result = [[0 for x in range(max_spent+1)] for x in range(len(actions)+1)]
+    for i in range(1, len(actions)+1):
+        for price in range(1, max_spent+1):
+            if actions[i-1][1] <= price:
+                result[i][price] = max(
+                    actions[i-1][2]*actions[i - 1][1] +
+                    result[i-1][price-actions[i-1][1]], result[i-1][price])
+            else:
+                result[i][price] = result[i-1][price]
+
+    s = max_spent
+    n = len(actions)
+    result_combination = []
+    while s >= 0 and n >= 0:
+        action = actions[n-1]
+        if result[n][s] == result[n-1][s - action[1]] + action[2]*action[1]:
+            result_combination.append(action)
+            s -= action[1]
+
+        n -= 1
+    return result_combination, MAX_SPENT-s, result[-1][-1]/100
+
+
+def display_best_combination(list_action, spent_action, gain_action):
+    """
+    Display best combination
+    :param
+        list_action: array best list actions
+    :param
+        spent_action:  integer
+    :param
+        gain_action: float
+    """
+    margin = 15
+    s = "Actions".ljust(margin, " ")+"Price".ljust(margin, " ")+"%".ljust(margin, " ")
+    for action in list_action:
+        s += "\n"+action[0].split("-")[1].ljust(margin, " ")
+        s += str(action[1]).ljust(margin, " ")
+        s += str(action[2]).ljust(margin, " ")
+    s += "\n\nMoney spent on stocks: {}".format(spent_action)
+    s += "\nTotal gain Net: {0:.2f}".format(gain_action)
+    s += "\nTotal gain Brut: {0:.2f}".format(gain_action+spent_action)
+    print(s)
+
+
+def main():
+    tracemalloc.start()
+    """ Read Data"""
+    data = read_data('actions.csv')
+    """ Return best combination, sum spent of combination, gain of combination"""
+    best_combination, spent, gain = find_best_combinations(MAX_SPENT, data)
+    """ Display best combination, sum spent, gain """
+    display_best_combination(best_combination, spent, gain)
+    snapshot = tracemalloc.take_snapshot()
+    top_stats = snapshot.statistics('lineno')
+    print("\n[ Top 4 memory usage ]\n")
+    for stat in top_stats[:4]:
+        print(stat)
+
+
+if __name__ == '__main__':
+    main()
